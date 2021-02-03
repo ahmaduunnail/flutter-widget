760364d6330fb3da0b4b7b39046b511bf40336c7
lleans <ahmadunnail@gmail.com>
2021-02-03 12:04:43 +0700

    ListView Dinamis data

--- lib/LIstView.dart
+++ lib/LIstView.dart
@@ -63,3 +63,31 @@ class ScrollingSreenStatis extends StatelessWidget {
     );
   }
 }
+
+class ScrollingDinamis extends StatelessWidget {
+  final List<int> numberList = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: numberList
+            .map(
+              (e) => Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Center(
+                  child: Text(
+                    "$e",
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -23,87 +23,93 @@ class App extends StatelessWidget {
       home: Scaffold(
         body: Center(
           widthFactor: 400,
-          child: Column(children: [
-            Container(height: 50, child: Code1()),
-            Container(
-              height: 50,
-              child: Code2(
-                text: "Hello World",
+          child: Column(
+            children: [
+              Container(height: 50, child: Code1()),
+              Container(
+                height: 50,
+                child: Code2(
+                  text: "Hello World",
+                ),
               ),
-            ),
-            Container(
-              height: 50,
-              child: NumberScreen(),
-            ),
-            Container(
-              height: 50,
-              child: ScaffoldWidget(),
-            ),
-            Container(
-              height: 50,
-              child: ContainerWidget(),
-            ),
-            Container(
-              height: 50,
-              child: ContainerDecoration(),
-            ),
-            Container(
-              height: 50,
-              child: PaddingWidget(),
-            ),
-            Container(
-              height: 50,
-              child: Rows(),
-            ),
-            Container(
-              height: 50,
-              child: Columns(),
-            ),
-            Container(
-              height: 50,
-              child: Stacks(),
-            ),
-            Container(
-              height: 50,
-              child: ButtonWidget(),
-            ),
-            Container(
-              height: 50,
-              child: inputtextfieldwidget(),
-            ),
-            Container(
-              height: 50,
-              child: SwitchInputWidget(),
-            ),
-            Container(
-              height: 50,
-              child: Radiobtn(),
-            ),
-            Container(
-              height: 50,
-              child: Checkboxbtn(),
-            ),
-            Container(
-              height: 50,
-              child: ImageLocal(),
-            ),
-            Container(
-              height: 50,
-              child: ImageServer(),
-            ),
-            Container(
-              height: 50,
-              child: ExpandedWidget(),
-            ),
-            Container(
-              height: 50,
-              child: FirstScreen(),
-            ),
-            Container(
-              height: 50,
-              child: ScrollingSreenStatis(),
-            ),
-          ]),
+              Container(
+                height: 50,
+                child: NumberScreen(),
+              ),
+              Container(
+                height: 50,
+                child: ScaffoldWidget(),
+              ),
+              Container(
+                height: 50,
+                child: ContainerWidget(),
+              ),
+              Container(
+                height: 50,
+                child: ContainerDecoration(),
+              ),
+              Container(
+                height: 50,
+                child: PaddingWidget(),
+              ),
+              Container(
+                height: 50,
+                child: Rows(),
+              ),
+              Container(
+                height: 50,
+                child: Columns(),
+              ),
+              Container(
+                height: 50,
+                child: Stacks(),
+              ),
+              Container(
+                height: 50,
+                child: ButtonWidget(),
+              ),
+              Container(
+                height: 50,
+                child: inputtextfieldwidget(),
+              ),
+              Container(
+                height: 50,
+                child: SwitchInputWidget(),
+              ),
+              Container(
+                height: 50,
+                child: Radiobtn(),
+              ),
+              Container(
+                height: 50,
+                child: Checkboxbtn(),
+              ),
+              Container(
+                height: 50,
+                child: ImageLocal(),
+              ),
+              Container(
+                height: 50,
+                child: ImageServer(),
+              ),
+              Container(
+                height: 50,
+                child: ExpandedWidget(),
+              ),
+              Container(
+                height: 50,
+                child: FirstScreen(),
+              ),
+              Container(
+                height: 50,
+                child: ScrollingSreenStatis(),
+              ),
+              Container(
+                height: 50,
+                child: ScrollingDinamis(),
+              ),
+            ],
+          ),
         ),
       ),
     );
