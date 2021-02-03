ef2ae6f7d568292c007cad8f6cca1cee580fd170
lleans <ahmadunnail@gmail.com>
2021-02-03 11:00:37 +0700

    Navigations

--- lib/main.dart
+++ lib/main.dart
@@ -23,76 +23,76 @@ class App extends StatelessWidget {
         body: Center(
           widthFactor: 400,
           child: Column(children: [
-            // Container(height: 50, child: Code1()),
-            // Container(
-            //   height: 50,
-            //   child: Code2(
-            //     text: "Hello World",
-            //   ),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: NumberScreen(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ScaffoldWidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ContainerWidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ContainerDecoration(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: PaddingWidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: Rows(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: Columns(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: Stacks(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ButtonWidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: inputtextfieldwidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: SwitchInputWidget(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: Radiobtn(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: Checkboxbtn(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ImageLocal(),
-            // ),Container(
-            //   height: 50,
-            //   child: ImageServer(),
-            // ),
-            // Container(
-            //   height: 50,
-            //   child: ExpandedWidget(),
-            // ),
+            Container(height: 50, child: Code1()),
+            Container(
+              height: 50,
+              child: Code2(
+                text: "Hello World",
+              ),
+            ),
+            Container(
+              height: 50,
+              child: NumberScreen(),
+            ),
+            Container(
+              height: 50,
+              child: ScaffoldWidget(),
+            ),
+            Container(
+              height: 50,
+              child: ContainerWidget(),
+            ),
+            Container(
+              height: 50,
+              child: ContainerDecoration(),
+            ),
+            Container(
+              height: 50,
+              child: PaddingWidget(),
+            ),
+            Container(
+              height: 50,
+              child: Rows(),
+            ),
+            Container(
+              height: 50,
+              child: Columns(),
+            ),
+            Container(
+              height: 50,
+              child: Stacks(),
+            ),
+            Container(
+              height: 50,
+              child: ButtonWidget(),
+            ),
+            Container(
+              height: 50,
+              child: inputtextfieldwidget(),
+            ),
+            Container(
+              height: 50,
+              child: SwitchInputWidget(),
+            ),
+            Container(
+              height: 50,
+              child: Radiobtn(),
+            ),
+            Container(
+              height: 50,
+              child: Checkboxbtn(),
+            ),
+            Container(
+              height: 50,
+              child: ImageLocal(),
+            ),Container(
+              height: 50,
+              child: ImageServer(),
+            ),
+            Container(
+              height: 50,
+              child: ExpandedWidget(),
+            ),
             Container(
               height: 250,
               child: FirstScreen(),
